c00cc8f10f862ecc2f5c94a5a48a2b1675b36877
luugiahuy123 <luugiahuyqn@gmail.com>
2023-12-14 16:36:53 +0700

    thuc hanh

---
+++ Circle.java
@@ -0,0 +1,34 @@
+package ss6.TH;
+
+public class Circle {
+    private double radius = 1.0;
+    public Circle(){
+    }
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+    public Circle(double radius, String color, boolean filled) {
+        super();
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+    public double getPerimeter(){
+        return 2*radius*Math.PI;
+    }
+    @Override
+    public String toString() {
+        return "A Circle with radius="
+                + getRadius()
+                + ", which is a subclass of "
+                + super.toString();
+    }
+
+}
